b2f88e7d3e47d0a4f97aa412705573c403218e2b
Мануэль Кегеян <kg.manyel@gmail.com>
2026-07-28 19:12:34 +0300

    fix: dependencies

--- modules/nixos/amd/amdgpu.nix
+++ modules/nixos/amd/amdgpu.nix
@@ -74,7 +74,8 @@
     enable = true;
     package = pkgs.lact.overrideAttrs (oldAttrs: {
       nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.pkg-config ];
-      buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.libdisplay-info ];
+      buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.libdisplay-info pkgs.libadwaita ];
+      propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ [ pkgs.libdisplay-info ];
     });
   };
 }
