8ff53418d0dc78079ab650865f7ecd1f5c30c61b
Мануэль Кегеян <kg.manyel@gmail.com>
2026-07-28 18:00:53 +0300

    fix: package all scripts together in store via mkDerivation with libexec

--- trustedinstaller/remote/drv.nix
+++ trustedinstaller/remote/drv.nix
@@ -13,10 +13,20 @@ pkgs.stdenvNoCC.mkDerivation {
   nativeBuildInputs = [ pkgs.makeWrapper ];
 
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -p $out/bin $out/libexec/local $out/libexec/remote
 
-    install -m 755 fetch.sh   $out/bin/fetch-remote
-    install -m 755 install.sh $out/bin/install-remote
+    install -m 755 ${../fetch-system.sh} $out/bin/fetch-target-device-paths
+    install -m 755 ${../local/fetch.sh}  $out/libexec/local/fetch.sh
+    install -m 755 ${../local/install.sh} $out/libexec/local/install.sh
+    install -m 755 fetch.sh             $out/libexec/remote/fetch.sh
+    install -m 755 install.sh           $out/libexec/remote/install.sh
+    install -m 755 fetch.sh             $out/bin/fetch-remote
+    install -m 755 install.sh           $out/bin/install-remote
+
+    wrapProgram $out/bin/fetch-target-device-paths \
+      --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
+        bash coreutils findutils gawk gnugrep gnused util-linux nix openssh
+      ])}
 
     wrapProgram $out/bin/install-remote \
       --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
