1474d157eb8e3b6b801585b3550cc7d1812c60d1
Мануэль Кегеян <kg.manyel@gmail.com>
2026-07-29 17:57:20 +0300

    fix: use removable grub installation

--- modules/nixos/bootloader/bootloader.nix
+++ modules/nixos/bootloader/bootloader.nix
@@ -6,7 +6,9 @@ in
   boot.supportedFilesystems = { btrfs = true; vfat = true; };
 
   boot.loader = {
-    efi.canTouchEfiVariables = true;
+    # Install GRUB at the UEFI fallback path (/EFI/BOOT/BOOTX64.EFI) instead
+    # of relying on a firmware NVRAM entry created by the live environment.
+    efi.canTouchEfiVariables = false;
     timeout = 5;
 
     grub = {
