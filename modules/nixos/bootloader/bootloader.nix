aa658a9d05438aa7c6a0e2a4bd514aa40059d548
Мануэль Кегеян <kg.manyel@gmail.com>
2026-07-29 15:31:02 +0300

    refactor: extract cold grub theme

--- modules/nixos/bootloader/bootloader.nix
+++ modules/nixos/bootloader/bootloader.nix
@@ -1,46 +1,6 @@
 { pkgs, ... }:
 let
-  nordicNixosGrubTheme = pkgs.runCommandLocal "grub-theme-nordic-nixos" { } ''
-    mkdir -p "$out"
-
-    cat > "$out/theme.txt" <<'EOF'
-    title-text: ""
-    desktop-image: "background.png"
-    desktop-color: "#2E3440"
-    message-font: "DejaVu Sans Bold 16"
-    message-color: "#D8DEE9"
-    terminal-font: "Unifont Regular"
-
-    + progress_bar {
-      id = "__timeout__"
-      left = 20%
-      top = 88%
-      width = 60%
-      height = 18
-      fg_color = "#88C0D0"
-      bg_color = "#3B4252"
-      border_color = "#4C566A"
-    }
-
-    + boot_menu {
-      left = 20%
-      top = 24%
-      width = 60%
-      height = 54%
-      item_font = "DejaVu Sans Bold 16"
-      item_color = "#D8DEE9"
-      item_height = 42
-      item_spacing = 6
-      item_padding = 8
-      selected_item_font = "DejaVu Sans Bold 16"
-      selected_item_color = "#88C0D0"
-      scrollbar = true
-      scrollbar_width = 8
-    }
-    EOF
-
-    ln -s ${pkgs.nixos-artwork.wallpapers.simple-dark-gray-bootloader.gnomeFilePath} "$out/background.png"
-  '';
+  coldNixosGrubTheme = import ./grub_theme.nix { inherit pkgs; };
 in
 {
   boot.supportedFilesystems = { btrfs = true; vfat = true; };
@@ -55,7 +15,7 @@ in
       efiSupport = true;
       gfxmodeEfi = "1920x1080,auto";
       gfxpayloadEfi = "keep";
-      theme = nordicNixosGrubTheme;
+      theme = coldNixosGrubTheme;
       backgroundColor = "#2E3440";
     };
   };
---
+++ modules/nixos/bootloader/grub_theme.nix
@@ -0,0 +1,43 @@
+{ pkgs }:
+
+pkgs.runCommandLocal "grub-theme-cold-nixos" { } ''
+  mkdir -p "$out"
+
+  cat > "$out/theme.txt" <<'EOF'
+  title-text: ""
+  desktop-image: "background.png"
+  desktop-color: "#2E3440"
+  message-font: "DejaVu Sans Bold 16"
+  message-color: "#D8DEE9"
+  terminal-font: "Unifont Regular"
+
+  + progress_bar {
+    id = "__timeout__"
+    left = 20%
+    top = 88%
+    width = 60%
+    height = 18
+    fg_color = "#88C0D0"
+    bg_color = "#3B4252"
+    border_color = "#4C566A"
+  }
+
+  + boot_menu {
+    left = 20%
+    top = 24%
+    width = 60%
+    height = 54%
+    item_font = "DejaVu Sans Bold 16"
+    item_color = "#D8DEE9"
+    item_height = 42
+    item_spacing = 6
+    item_padding = 8
+    selected_item_font = "DejaVu Sans Bold 16"
+    selected_item_color = "#88C0D0"
+    scrollbar = true
+    scrollbar_width = 8
+  }
+  EOF
+
+  ln -s ${pkgs.nixos-artwork.wallpapers.simple-dark-gray-bootloader.gnomeFilePath} "$out/background.png"
+''
